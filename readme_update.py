31d4faa43981868008f0e581584b06878232c057
RezaRjbi <reza.rajabi61925@gmail.com>
2021-08-14 12:11:56 +0430

    commit message20

--- readme_update.py
+++ readme_update.py
@@ -1,4 +1,19 @@
-import datetime
+import pathlib
+import os
+import shutil
+import nbformat
+from nbconvert import HTMLExporter
+PROJECTS_PATH = pathlib.Path('projects')
+html_exporter = HTMLExporter()
+html_exporter.template_name = 'classic'
+ipynb_docs = list(PROJECTS_PATH.glob('**/*.ipynb'))
 
-with open('README.md', 'w') as rm:
-    rm.write(f'{datetime.datetime.now()}')
+for ipynb in ipynb_docs:
+    directory, file = os.path.split(ipynb)
+    result_html = f'{os.path.splitext(file)[0]}.html'
+    result_html_path = os.path.join(directory, result_html)
+    nb = nbformat.reads(ipynb.read_text(), as_version=4)
+    body, resources = html_exporter.from_notebook_node(nb)
+    os.system(f'jupyter nbconvert --to html {ipynb.absolute()}')
+    shutil.move(result_html_path, os.path.join('docs', result_html))
+
